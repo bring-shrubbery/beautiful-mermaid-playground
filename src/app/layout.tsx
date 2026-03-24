7c8f0b83eb9cf4bb0e1f1c566c8450da15824c4a
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-03-24 14:18:50 +0200

    setup shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import "@/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
 	title: "Create T3 App",
@@ -18,7 +21,7 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html className={`${geist.variable}`} lang="en">
+		<html className={cn(geist.variable, "font-sans", inter.variable)} lang="en">
 			<body>{children}</body>
 		</html>
 	);
